04254ef381628e188e21b9dd60e9ed40ec99a7a4
Schoen Cho <fourthbus@gmail.com>
2020-05-16 04:11:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import geopandas as gpd
+import pandas as pd
 from geopandas.geoseries import *
 from shapely import wkt
+from shapely.geometry import box
+
+# converting row input of four min/max to rectangle vertices
+def retangulator (ip):
+    rect = box(ip[0], ip[1], ip[2], ip[3])
+    return rect
 
 # gigl = gpd.read_file("GIGL/GIGL_OPS_filtered.shp")
 road = gpd.read_file("Road/London road link_subtracted_Segment_Map.shp")
@@ -14,6 +21,35 @@ park = gpd.read_file("GIGL/GiGL_OPS_filtered_CopyFeatur.shp")
 roadbound = road.geometry.bounds
 parkbound = park.geometry.bounds
 
-parkbound = parkbound.add(pd.Series([-50,-50,50,50], index=['minx','miny','maxx','maxy']), axis='index')
+parkbound_judge = parkbound.add(pd.Series([-25,-25,25,25], index=['minx','miny','maxx','maxy']))
 
-parkbound_judge = parkbound.sub(pd.Series([-50,-50,50,50], index=['minx','miny','maxx','maxy']))
+
+
+# initialising rectdata
+rectdata = pd.DataFrame(columns=['geometry'])
+rectdata['geometry'] = parkbound_judge.apply(retangulator,axis=1)
+# convert rectdata to GeoDataframe
+rectdata = gpd.GeoDataFrame(rectdata, geometry='geometry')
+
+def inthebox (roadboundrow,rectrow):
+    rbminx = roadboundrow[0]
+    rbminy = roadboundrow[1]
+    rbmaxx = roadboundrow[2]
+    rbmaxy = roadboundrow[3]
+    reminx = rectrow[0]
+    reminy = rectrow[1]
+    remaxx = rectrow[2]
+    remaxy = rectrow[3]
+    if reminx < rbminx < remaxx or reminx < rbmaxx < remaxx:
+        xin = True
+    else:
+        xin = False
+    if reminy < rbminy < remaxy or reminy < rbmaxy < remaxy:
+        yin = True
+    else:
+        yin = False
+        
+    
+
+    
+rbsample.apply(inthebox,axis=1)
